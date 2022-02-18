8e04fc3ad778b06e2ba93a6aaffc8d295b396234
carlosdaniel77 <cdg4305@gmail.com>
2022-02-18 16:24:00 -0300

    git add -A git commit -m "Carlos" git push origin main

---
+++ SistemaAcademico/SistemaAcademico/Model/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Model
+{
+    class Pessoa
+    {
+        //Atributos - Variáveis de Classe
+        public string nome;
+        public long cpf;
+        public DateTime dataNascimento;
+        public long endereco;
+        public int telefone; 
+    }
+}
